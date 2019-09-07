5ad59c22fad23b47b308fe5b27f7512dfcd4c148
Nikita Holovnin <Golovninnikita@ukr.net>
2019-09-07 16:35:48 +0300

    task=6 config

--- goit-js-hw-01/js/task-6.js
+++ goit-js-hw-01/js/task-6.js
@@ -1,22 +1,21 @@
+/* eslint-disable no-continue */
 /* eslint-disable no-constant-condition */
 /* eslint-disable no-alert */
 let input;
 let total = 0;
+const ENTER_NUMBER = 'Введите число';
+const NO_NUMBER = 'Было введено не число, попробуйте еще раз';
 
 while (input !== null) {
-  input = prompt('Введите число');
-
-  if (input === null) {
-    alert(`Общая сумма чисел равна ${total}`);
-    console.log(`Общая сумма чисел равна ${total}`);
-    break;
-  }
-  input = Number.parseFloat(input);
-
-  total += input;
-
-  if (Number.isNaN(input)) {
-    alert('Было введено не число, попробуйте еще раз');
-    console.log('Было введено не число, попробуйте еще раз');
+  input = prompt(ENTER_NUMBER, 0);
+  const userNumber = +input;
+  if (Number.isNaN(userNumber) && input !== null) {
+    alert(NO_NUMBER);
+    console.log(NO_NUMBER);
+    continue;
+  } else {
+    total += userNumber;
   }
 }
+const RESULT = `Общая сумма чисел равна ${total}`;
+alert(RESULT);
