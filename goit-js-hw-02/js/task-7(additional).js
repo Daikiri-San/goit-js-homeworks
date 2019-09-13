d0a54b08158d30c240e508e37ee6bd76a186c26a
Nikita Holovnin <Golovninnikita@ukr.net>
2019-09-14 01:20:38 +0300

    additional task hw-2

--- goit-js-hw-02/js/task-7(additional).js
+++ goit-js-hw-02/js/task-7(additional).js
@@ -1,16 +1,29 @@
 /* eslint-disable func-names */
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const LENGTH_ERROR = 'Ошибка! Логин должен быть от 4 до 16 символов';
+const PASSWORD__RESERVED = 'Такой логин уже используется!';
+const PASSWORD__SUCCESS = 'Логин успешно добавлен!';
 
 const isLoginValid = function (login) {
-  // код
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
+  }
+  return false;
 };
 
 const isLoginUnique = function (allLogins, login) {
-  // код
+  return allLogins.includes(login);
 };
 
 const addLogin = function (allLogins, login) {
-  // код
+  if (isLoginValid(login) === false) {
+    return console.log(LENGTH_ERROR);
+  }
+  if (isLoginUnique(allLogins, login) === true) {
+    return console.log(PASSWORD__RESERVED);
+  }
+  allLogins.push(login);
+  return console.log(PASSWORD__SUCCESS);
 };
 
 // Вызовы функции для проверки
