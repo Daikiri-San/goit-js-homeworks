d25f66aec7a8327ba1b7ae9d06b947d60a6910e8
Nikita Holovnin <Golovninnikita@ukr.net>
2019-10-30 18:51:39 +0200

    module 8 js-hw

--- goit-js-hw-07/js/task-8(additional).js
+++ goit-js-hw-07/js/task-8(additional).js
@@ -1,45 +1,62 @@
+/* eslint-disable consistent-return */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-multi-spaces */
 /* eslint-disable no-unused-vars */
 const refs = {
-  amount: document.querySelector('#controls'),
-  divs: document.querySelector('#boxes'),
   input: document.querySelector('input[type="number"]'),
+  create: document.querySelector('button[data-action="render"]'),
+  destroy: document.querySelector('button[data-action="destroy"]'),
+  boxes: document.querySelector('#boxes'),
 };
 
-function createBoxes(amount) {
-  let element = '';
-  if (amount > 0 && amount <= 100) {
-    for (let i = 0; i < amount; i += 1) {
-      const rgbRedNumber = Math.floor(Math.random() * 255);
-      const rgbGreenNumber = Math.floor(Math.random() * 255);
-      const rgbBlueNumber = Math.floor(Math.random() * 255);
-
-      const rgb = `rgb(${rgbRedNumber}, ${rgbGreenNumber}, ${rgbBlueNumber})`;
-      const size = (i + 3) * 10;
-
-      element += `
-        <div style="background-color: ${rgb}; width:${size}px; height:${size}px;"></div>
-        `;
-    }
-  }
-
-  return element;
+function getRandomColor() {
+  return Math.floor(Math.random() * 256);
 }
 
-const makeActions = {
-  render() {
-    refs.divs.insertAdjacentHTML('beforeend', createBoxes(refs.input.value));
-  },
+function gerRandomRGB() {
+  return `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
+}
 
-  destroy() {
-    refs.divs.innerHTML = '';
-  },
-};
-/* 'if' here for not cause error in console, when click
- was on something that is not a button */
-const handleClick = ({ target }) => {
-  if (target.tagName === 'BUTTON') {
-    makeActions[target.dataset.action]();
+const SIZE = 30;
+
+function createBoxes(amount) {
+  let div;
+  if (amount > 0 && amount <= 100) {
+    if (refs.boxes.lastChild === null) {
+      return Array(amount)
+        .fill('')
+        .map((e, i) => {
+          const attr = i;
+          div = document.createElement('div');
+          div.setAttribute('data-attr', attr);
+          div.style.cssText = `width: ${SIZE + i * 10}px; height: ${SIZE
+            + i * 10}px; background-color: ${gerRandomRGB()}`;
+          return div;
+        });
+    }
+    return Array(amount)
+      .fill('')
+      .map((e, i) => {
+        div = document.createElement('div');
+        const prevIndex =          i + 1 + Number(refs.boxes.lastElementChild.dataset.attr);
+        const varForSize =          i + 1 + 3 + Number(refs.boxes.lastElementChild.dataset.attr);
+        const dataAttr = prevIndex;
+        const size = varForSize * 10;
+        div.setAttribute('data-attr', dataAttr);
+        div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${gerRandomRGB()}`;
+        return div;
+      });
   }
-};
+  return div;
+}
 
-refs.amount.addEventListener('click', handleClick);
+function setBoxes() {
+  const num = +refs.input.value;
+  refs.boxes.append(...createBoxes(num));
+}
+function destroyBoxes() {
+  refs.boxes.innerHTML = '';
+  refs.input.value = '';
+}
+refs.create.addEventListener('click', setBoxes);
+refs.destroy.addEventListener('click', destroyBoxes);
---
+++ goit-js-hw-08/js/app.js
@@ -0,0 +1,131 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-lone-blocks */
+import galleryItems from './gallery-items.js';
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  button: document.querySelector('button[data-action="close-lightbox"]'),
+  imageLightbox: document.querySelector('img.lightbox__image'),
+};
+
+function createMarkup(array) {
+  let wholeMarkup = '';
+  array.forEach((element) => {
+    const { preview, original, description } = element;
+    wholeMarkup += `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src=""
+        data-lazy="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+  
+      <span class="gallery__icon">
+        <i class="material-icons">zoom_out_map</i>
+      </span>
+    </a>
+  </li>`;
+  });
+  return wholeMarkup;
+}
+
+const markup = createMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+function openModal(event) {
+  event.preventDefault();
+  const { target } = event;
+  if (target.className !== 'gallery__image') {
+    return;
+  }
+  refs.imageLightbox.setAttribute('src', target.dataset.source);
+  refs.imageLightbox.setAttribute('alt', target.alt);
+  refs.lightbox.classList.add('is-open');
+}
+
+function closeModal() {
+  refs.lightbox.classList.remove('is-open');
+  refs.imageLightbox.removeAttribute('src');
+  refs.imageLightbox.removeAttribute('alt');
+}
+
+function closeModalByClick({ target }) {
+  if (target.className !== 'lightbox__content') {
+    return;
+  }
+  refs.lightbox.classList.remove('is-open');
+  refs.imageLightbox.removeAttribute('src');
+  refs.imageLightbox.removeAttribute('alt');
+}
+
+function closeModalByEsc(event) {
+  if (event.keyCode !== 27) {
+    return;
+  }
+  refs.lightbox.classList.remove('is-open');
+  refs.imageLightbox.removeAttribute('src');
+  refs.imageLightbox.removeAttribute('alt');
+}
+
+function nextImg(event) {
+  if (event.keyCode === 37 || event.keyCode === 39) {
+    const currentValues = refs.imageLightbox;
+    if (!currentValues.hasAttribute('src')) {
+      return;
+    }
+    const { src } = currentValues;
+    const array = Array.from(galleryItems);
+    const current = array.findIndex(el => el.original === src);
+    let next = current;
+    if (current !== -1) {
+      if (event.keyCode === 37) next -= 1;
+      if (event.keyCode === 39) next += 1;
+      if (next <= 0) next = 0;
+      if (next >= array.length - 1) next = array.length - 1;
+      refs.imageLightbox.removeAttribute('src');
+      refs.imageLightbox.removeAttribute('alt');
+      const { original, description } = array[next];
+      refs.imageLightbox.setAttribute('src', original);
+      refs.imageLightbox.setAttribute('alt', description);
+    }
+  }
+}
+
+const lazyLoad = (target) => {
+  const options = {
+    rootMargin: '300px 0px',
+    threshold: 0.01,
+  };
+
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const imageUrl = img.dataset.lazy;
+
+        img.setAttribute('src', imageUrl);
+        img.classList.add('fade-in');
+
+        observer.disconnect();
+      }
+    });
+  }, options);
+  io.observe(target);
+};
+
+refs.imeges = document.querySelectorAll('.gallery__image');
+refs.imeges.forEach((image) => {
+  lazyLoad(image);
+});
+
+refs.gallery.addEventListener('click', openModal);
+refs.button.addEventListener('click', closeModal);
+refs.lightbox.addEventListener('click', closeModalByClick);
+window.document.addEventListener('keydown', closeModalByEsc);
+window.document.addEventListener('keydown', nextImg);
