a450b6bac906bec1b36b2e999c540c5b5e74c0c2
Nikita Holovnin <Golovninnikita@ukr.net>
2019-12-10 14:19:48 +0200

    config module 13 hw-js on ESC eventListener in basicLightBox

--- goit-js-hw-13-image-finder/src/components/vendors/lightBox.js
+++ goit-js-hw-13-image-finder/src/components/vendors/lightBox.js
@@ -8,12 +8,14 @@ function openLightBox({ target }) {
   const image = `<img class="modalImg" src="${largeImg}" width="800" height="600">`;
   const instance = basicLightbox.create(image);
   instance.show();
-  document.body.addEventListener('keydown', (event) => {
+  function closeLightBox(event) {
     if (event.keyCode !== 27) {
       return;
     }
     instance.close();
-  });
+    document.body.removeEventListener('keydown', closeLightBox);
+  }
+  document.body.addEventListener('keydown', closeLightBox);
 }
 
 export default openLightBox;
